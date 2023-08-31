2b641ec81a3162718a6e29b947ffc1ad41463cec
Conrad 宮文軒 <mackayleblanc.conrad@gmail.com>
2023-08-31 20:00:42 +0200

    fixed problem

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,3 +1,73 @@
+// import { useContext } from "react";
+// import { Link } from "react-router-dom";
+// import Logo from "./../assets/logo-officiel-Aptenna.png";
+// import "./../styles/Navbar.css";
+// import { UserContext } from "../context/authContext";
+// import Logout from "./../assets/icons-logs/logout.png";
+// import Login from "./../assets/icons-logs/login.jpg";
+
+// function Navbar() {
+//     const { isLoggedIn, authenticateUser } = useContext(UserContext); // Utilisation du hook useAuth
+
+//     function logout() {
+//         localStorage.removeItem("token");
+//         authenticateUser();
+//     }
+
+//     return (
+//         <nav className="navbar">
+//             <div className="logo">
+//                 {/* Place your logo image here */}
+//                 <img src={Logo} alt="Logo" />
+//             </div>
+//             <div className="navbar-onglet">
+//                 <ul className="nav-links">
+//                     <li>
+//                         <Link className="onglet-navbar-link link-underline" to="/">
+//                             Home
+//                         </Link>
+//                     </li>
+//                     <li>
+//                         <Link className="onglet-navbar-link link-underline" to="/team">
+//                             Team
+//                         </Link>
+//                     </li>
+//                     <li>
+//                         <Link className="onglet-navbar-link link-underline" to="/prestation">
+//                             Prestation
+//                         </Link>
+//                     </li>
+//                     <li>
+//                         <Link className="onglet-navbar-link link-underline" to="/Contact">
+//                             Contact
+//                         </Link>
+//                     </li>
+//                 </ul>
+//                 <ul className="auth-links">
+//                     {isLoggedIn ? (
+//                         // Afficher Logout si l'utilisateur est connecté
+//                         <li>
+//                             <button className="onglet-navbar-link link-underline logout-icon" onClick={logout}>
+//                                 <img className="img-logout" src={Logout} alt="Logo" />
+//                             </button>
+//                         </li>
+//                     ) : (
+//                         // Afficher Login si l'utilisateur n'est pas connecté
+//                         <li>
+//                             <Link className="onglet-navbar-link link-underline" to="/login">
+//                                 <img className="img-login" src={Login} alt="Logo" />
+//                             </Link>
+//                             </li>
+
+//                     )}
+//                 </ul>
+//             </div>
+//         </nav>
+//     );
+// }
+
+// export default Navbar;
+
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import Logo from "./../assets/logo-officiel-Aptenna.png";
@@ -7,7 +77,7 @@ import Logout from "./../assets/icons-logs/logout.png";
 import Login from "./../assets/icons-logs/login.jpg";
 
 function Navbar() {
-    const { isLoggedIn, authenticateUser } = useContext(UserContext); // Utilisation du hook useAuth
+    const { isLoggedIn, authenticateUser } = useContext(UserContext);
 
     function logout() {
         localStorage.removeItem("token");
@@ -17,7 +87,6 @@ function Navbar() {
     return (
         <nav className="navbar">
             <div className="logo">
-                {/* Place your logo image here */}
                 <img src={Logo} alt="Logo" />
             </div>
             <div className="navbar-onglet">
@@ -45,19 +114,24 @@ function Navbar() {
                 </ul>
                 <ul className="auth-links">
                     {isLoggedIn ? (
-                        // Afficher Logout si l'utilisateur est connecté
                         <li>
                             <button className="onglet-navbar-link link-underline logout-icon" onClick={logout}>
-                                <img className="img-logout" src={Logout} alt="Logo" />
+                                <img className="img-logout" src={Logout} alt="Logout" />
                             </button>
                         </li>
                     ) : (
-                        // Afficher Login si l'utilisateur n'est pas connecté
-                        <li>
-                            <Link className="onglet-navbar-link link-underline" to="/login">
-                                <img className="img-login" src={Login} alt="Logo" />
-                            </Link>
-                        </li>
+                        <>
+                            <li>
+                                <Link className="onglet-navbar-link link-underline" to="/login">
+                                    <img className="img-login" src={Login} alt="Login" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link className="onglet-navbar-link link-underline" to="/signup">
+                                    Sign Up
+                                </Link>
+                            </li>
+                        </>
                     )}
                 </ul>
             </div>
